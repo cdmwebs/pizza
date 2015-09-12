298d3d08444508a7393efd776b8a2ab2ade40185
Chris Moore <chrism@gaslight.co>
2015-09-11 22:33:12 -0400

    Add location to venues

--- app/mirage/factories/venue.js
+++ app/mirage/factories/venue.js
@@ -2,5 +2,6 @@ import Mirage, {faker} from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
   name(i) { return `Pizza Place ${i}`; },
-  avatar: faker.internet.avatar
+  avatar: faker.internet.avatar,
+  location: L.latLng(37.77493, -122.419415),
 });
--- app/models/venue.js
+++ app/models/venue.js
@@ -4,6 +4,7 @@ var attr = DS.attr;
 
 export default DS.Model.extend({
   name: attr(),
-  avatar: attr()
+  avatar: attr(),
+  location: attr()
 });
 
