f16ba693cd0d59a04b7155dfa894c1e3c5337968
Chris Moore <chrism@gaslight.co>
2015-09-12 11:02:18 -0400

    Use fixtures in favor of factories

--- app/mirage/factories/venue.js
+++ app/mirage/factories/venue.js
@@ -1,7 +1,7 @@
 import Mirage, {faker} from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
-  name(i) { return `Pizza Place ${i + 1}`; },
+  name() { return faker.company.companyName(); },
   avatar: faker.internet.avatar,
-  location(i) { return L.latLng(`${39.1000 + (i / 100)}`, -84.5167); },
+  location(i) { return L.latLng(`${39.1000 + (i / 1000)}`, -84.5167 + (i/1000)); },
 });
---
+++ app/mirage/fixtures/venues.js
@@ -0,0 +1,80 @@
+export default [
+  {
+    name: "Strong's Brick Oven Pizzeria",
+    address: "336 Monmouth St, Newport, KY 41071",
+    location: L.latLng(39.093486, -84.495303)
+  }, {
+    name: "Goodfella's Pizza",
+    address: "603 Main St, Covington, KY 41011",
+    location: L.latLng(39.084084, -84.517512)
+  }, {
+    name: "Ramundo's Pizzeria",
+    address: "3166 Linwood Ave, Cincinnati, OH 45208",
+    location: L.latLng(39.129954, -84.429824)
+  }, {
+    name: "Lucy Blue's Pizza",
+    address: "1126 Main St, Cincinnati, OH 45202",
+    location: L.latLng(39.108089, -84.511300)
+  }, {
+    name: "A Tavola",
+    address: "1220 Vine St, Cincinnati, OH 45202",
+    location: L.latLng(39.108305, -84.514625)
+  }, {
+    name: "Pi Pizzeria",
+    address: "199 E 6th St, Cincinnati, OH 45202",
+    location: L.latLng(39.102660, -84.509843)
+  }, {
+    name: "Adriatico's",
+    address: "113 W McMillan St, Cincinnati, OH 45219",
+    location: L.latLng(39.127322, -84.516088)
+  }, {
+    name: "Fireside Pizza",
+    address: "773 E McMillan St, Cincinnati, OH 45206",
+    location: L.latLng(39.125660, -84.491543)
+  }, {
+    name: "Dewey's Pizza",
+    address: "265 Hosea Ave, Cincinnati, OH 45220",
+    location: L.latLng(39.143593, -84.517903)
+  }, {
+    name: "J Bar Pizzeria",
+    address: "2637 Erie Ave, Cincinnati, OH 45208",
+    location: L.latLng(39.139760, -84.443987)
+  }, {
+    name: "Jean Paul's Paradiso",
+    address: "6732 Clough Pike, Cincinnati, OH 45244",
+    location: L.latLng(39.097474, -84.368178)
+  }, {
+    name: "Trotta's Pizza & Drive Thru",
+    address: "3501 Werk Rd, Cincinnati, OH 45248",
+    location: L.latLng(39.141259, -84.618146)
+  }, {
+    name: "Angilo's Pizza",
+    address: "6953 Harrison Ave, Cincinnati, OH 45247",
+    location: L.latLng(39.205493, -84.673282)
+  }, {
+    name: "Pizzelii",
+    address: "7639 Wooster Pike, Cincinnati, OH 45227",
+    location: L.latLng(39.138946, -84.354091)
+  }, {
+    name: "Chester's Pizzeria",
+    address: "2929 Dixie Hwy, Hamilton, OH 45015",
+    location: L.latLng(39.366948, -84.545812)
+  }, {
+    name: "Flying Pizza",
+    address: "9563 Fields Ertel Rd, Loveland, OH 45140",
+    location: L.latLng(39.290928, -84.290768)
+  }, {
+    name: "Rocafella's Pizza",
+    address: "11055 Reading Rd Sharonville, OH 45241",
+    location: L.latLng(39.269998, -84.412656)
+  }, {
+    name: "Rudinos Pizza & Grinders",
+    address: "9730 Montgomery Rd, Cincinnati, OH 45242",
+    location: L.latLng(39.232613, -84.351509)
+  }, {
+    name: "Garzelli's Grinder & Pizza Co",
+    address: "3025 Dixie Hwy, Edgewood, KY 41017",
+    location: L.latLng(39.023012, -84.585065)
+  }
+];
+
