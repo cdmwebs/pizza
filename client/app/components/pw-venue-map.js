a4d684336e12c8f24cd649427f987e7a6795d425
Chris Moore <chrism@gaslight.co>
2015-09-12 23:44:22 -0400

    Clean up maps

--- client/app/components/pw-venue-map.js
+++ client/app/components/pw-venue-map.js
@@ -1,21 +1,38 @@
 import EmberLeafletComponent from 'ember-leaflet/components/leaflet-map';
 import MarkerLayer from 'ember-leaflet/layers/marker';
 import TileLayer from 'ember-leaflet/layers/tile';
+import MarkerCollectionLayer from 'ember-leaflet/layers/marker-collection';
+import CollectionBoundsMixin from 'ember-leaflet/mixins/collection-bounds';
+import PopupMixin from 'ember-leaflet/mixins/popup';
+
+var markerLayer = MarkerLayer.extend(PopupMixin, {
+  popupContentBinding: 'content.venue.name'
+});
+
+var markersLayer = MarkerCollectionLayer.extend(CollectionBoundsMixin, {
+  itemLayerClass: markerLayer,
+  contentBinding: 'controller.locations',
+  addChildLayer: function(layer) {
+    this._super(layer);
+    var bounds = this.get('bounds');
+    var parentLayer = this.get('parentLayer.layer');
+    parentLayer.fitBounds(bounds);
+  },
+});
+
+var tileLayer = TileLayer.extend({
+  tileUrl: 'https://api.mapbox.com/v4/{style}/{z}/{x}/{y}.png?access_token={token}',
+  options: {
+    style: 'mapbox.streets',
+    token: 'pk.eyJ1IjoiY2Rtd2VicyIsImEiOiJjaWVnaHdsNXowMDN2czZtM3g2Nm9zMHJkIn0.ZyY3UxiPh5eepFIKeYY-Xg'
+  }
+});
 
 export default EmberLeafletComponent.extend({
   zoom: 14,
-  center: Ember.computed.alias('controller.venue.location'),
+  detectRetina: true,
   childLayers: [
-    TileLayer.extend({
-      tileUrl: 'https://api.mapbox.com/v4/{style}/{z}/{x}/{y}.png?access_token={token}',
-      options: {
-        style: 'mapbox.streets',
-        token: 'pk.eyJ1IjoiY2Rtd2VicyIsImEiOiJjaWVnaHdsNXowMDN2czZtM3g2Nm9zMHJkIn0.ZyY3UxiPh5eepFIKeYY-Xg'
-      }
-    }),
-
-    MarkerLayer.extend({
-      location: Ember.computed.alias('controller.venue.location'),
-    })
+    tileLayer,
+    markersLayer
   ]
 });
--- client/app/components/pw-venues-map.js
+++ client/app/components/pw-venues-map.js
@@ -5,31 +5,35 @@ import CollectionBoundsMixin from 'ember-leaflet/mixins/collection-bounds';
 import PopupMixin from 'ember-leaflet/mixins/popup';
 import TileLayer from 'ember-leaflet/layers/tile';
 
+var markerLayer = MarkerLayer.extend(PopupMixin, {
+  popupContentBinding: 'content.venue.name'
+});
+
+var markersLayer = MarkerCollectionLayer.extend(CollectionBoundsMixin, {
+  itemLayerClass: markerLayer,
+  contentBinding: 'controller.locations',
+  didCreateLayer: function(layer) {
+    this._super();
+    var bounds = this.get('bounds');
+    var parentLayer = this.get('parentLayer.layer');
+    parentLayer.fitBounds(bounds);
+  },
+});
+
+var tileLayer = TileLayer.extend({
+  tileUrl: 'https://api.mapbox.com/v4/{style}/{z}/{x}/{y}.png?access_token={token}',
+  options: {
+    style: 'mapbox.streets',
+    token: 'pk.eyJ1IjoiY2Rtd2VicyIsImEiOiJjaWVnaHdsNXowMDN2czZtM3g2Nm9zMHJkIn0.ZyY3UxiPh5eepFIKeYY-Xg'
+  }
+});
+
 export default EmberLeafletComponent.extend({
   zoom: 14,
   center: L.latLng(39.1000, -84.5167),
   detectRetina: true,
   childLayers: [
-    TileLayer.extend({
-      tileUrl: 'https://api.mapbox.com/v4/{style}/{z}/{x}/{y}{format}?access_token={token}',
-      options: {
-        format: '@2x.png',
-        style: 'mapbox.streets',
-        token: 'pk.eyJ1IjoiY2Rtd2VicyIsImEiOiJjaWVnaHdsNXowMDN2czZtM3g2Nm9zMHJkIn0.ZyY3UxiPh5eepFIKeYY-Xg'
-      }
-    }),
-
-    MarkerCollectionLayer.extend(CollectionBoundsMixin, {
-      contentBinding: 'controller.venues',
-      itemLayerClass: MarkerLayer.extend(PopupMixin, {
-        popupContentBinding: 'content.name'
-      }),
-      didCreateLayer: function() {
-        this._super();
-        if (this.get('controller.venues.length') > 0) {
-          this.get('parentLayer').get('layer').fitBounds(this.get('bounds'));
-        }
-      }
-    })
+    tileLayer,
+    markersLayer
   ]
 });
