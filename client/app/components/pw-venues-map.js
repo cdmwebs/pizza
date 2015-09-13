83dd6397b261f1104354429d49ac8557f53a31f2
Chris Moore <chrism@gaslight.co>
2015-09-12 21:07:40 -0400

    Load retina map tiles

--- client/app/components/pw-venues-map.js
+++ client/app/components/pw-venues-map.js
@@ -8,10 +8,12 @@ import TileLayer from 'ember-leaflet/layers/tile';
 export default EmberLeafletComponent.extend({
   zoom: 14,
   center: L.latLng(39.1000, -84.5167),
+  detectRetina: true,
   childLayers: [
     TileLayer.extend({
-      tileUrl: 'https://api.mapbox.com/v4/{style}/{z}/{x}/{y}.png?access_token={token}',
+      tileUrl: 'https://api.mapbox.com/v4/{style}/{z}/{x}/{y}{format}?access_token={token}',
       options: {
+        format: '@2x.png',
         style: 'mapbox.streets',
         token: 'pk.eyJ1IjoiY2Rtd2VicyIsImEiOiJjaWVnaHdsNXowMDN2czZtM3g2Nm9zMHJkIn0.ZyY3UxiPh5eepFIKeYY-Xg'
       }
